b35211d56e51fbe4dbf2c6fb2fedb417e54101f3
AzureAD\DimasMaendraUtomo <dimas.utomo@anteraja.id>
2025-01-30 14:18:54 +0700

    starting logger

---
+++ src/test/java/com/example/java/logging/JavaLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
